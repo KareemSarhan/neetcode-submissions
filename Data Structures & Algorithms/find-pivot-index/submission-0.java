838fdfc18d1bf6391ef06cad8ab303cff22bb489
Kareem Sarhan <kareemsarhan@gmail.com>
2026-08-11 22:03:49 +0300

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        if (nums.length==1) return 0;
+        int [] l = new int [nums.length+1];
+        int [] r = new int [nums.length+1];
+        for(int i=0;i<nums.length;i++)
+        {
+            l[i+1]=l[i]+nums[i];
+            r[nums.length-i-1]=r[nums.length-i]+nums[nums.length-i-1];
+        }
+        System.out.println(Arrays.toString(r));
+        System.out.println(Arrays.toString(l));
+        for(int i=0;i<l.length-1;i++)
+        {
+            if(l[i]==r[i+1]) return i;
+        }
+        return -1;
+    }
+}
